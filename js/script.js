89dbf7dfc6f6699a81c7cc13ac3b33710c67d058
bfly2001 <brendan.sflynn@gmail.com>
2019-08-18 15:42:13 -0700

    generated HTML legend from js
    generated HTML legend from js

--- js/script.js
+++ js/script.js
@@ -52,10 +52,42 @@ var tag=document.getElementById(el);
             }
           },
       legend: {
-              position: 'top',
+            display: false
            }
         }
   });
+
+var myLegendContainer = document.getElementById("line-graph_legend");
+myLegendContainer.innerHTML = trafficChart.generateLegend();
+var legendItems = myLegendContainer.getElementsByTagName('li');
+for (var i = 0; i < legendItems.length; i += 1) {
+  legendItems[i].addEventListener("click", legendClickCallback, false);
+}
+
+function legendClickCallback(event) {
+  event = event || window.event;
+
+  var target = event.target || event.srcElement;
+  while (target.nodeName !== 'LI') {
+    target = target.parentElement;
+  }
+  var parent = target.parentElement;
+  var chartId = parseInt(parent.classList[0].split("-")[0], 10);
+  var chart = Chart.instances[chartId];
+  var index = Array.prototype.slice.call(parent.children).indexOf(target);
+  var meta = chart.getDatasetMeta(0);
+  console.log(index);
+	var item = meta.data[index];
+
+  if (item.hidden === null || item.hidden === false) {
+    item.hidden = true;
+    target.classList.add('hidden');
+  } else {
+    target.classList.remove('hidden');
+    item.hidden = null;
+  }
+  chart.update();
+}
 
   let myChart2 = document.getElementById('myChart2').getContext('2d');
 
